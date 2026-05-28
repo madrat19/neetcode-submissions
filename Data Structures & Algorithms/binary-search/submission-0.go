e94e7226d2a3e6661a609a24ac468bf4e909f293
Alexey <alexeyz97@mail.ru>
2026-05-28 11:24:21 +0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.go
@@ -0,0 +1,16 @@
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l < r {
+		m := (l + r) / 2
+		if nums[m] >= target {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	if nums[l] != target {
+		l = -1
+	}
+	return l
+}
